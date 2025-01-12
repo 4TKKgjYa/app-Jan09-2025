e36f3243f8d7cfbb2fe3689cf2b8e5cf37c16e93
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2025-01-12 21:51:22 +0900

    shadcn/uiの使用例

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Link href="/command">Command</Link>
+      <Button>送信</Button>
     </div>
   );
 }
