d4b881d88986dfab7503206ea16911c4b0209902
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2025-01-21 23:32:49 +0900

    feature: Resendでのメール送信機能の続き

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -4,7 +4,8 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(request: Request) {
+  const { userName, subject, email, content } = await request.json();
   try {
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
